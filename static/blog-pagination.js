43a66868bead044227e30bcc0b928e4d1d57327e
peonily <abdallah.1997971@gmail.com>
2026-05-07 01:20:23 +0100

    Fix blog thumbnail sizing

--- static/blog-pagination.js
+++ static/blog-pagination.js
@@ -1,5 +1,23 @@
 (function () {
   const POSTS_PER_PAGE = 21;
+  const style = document.createElement("style");
+  style.textContent = `
+    .postCard__thumb{
+      width:100%;
+      aspect-ratio:16 / 9;
+      margin:0 0 14px;
+      border-radius:16px;
+    }
+    .postCard__thumb img{
+      object-fit:cover;
+    }
+    .postCard__thumb img[src^="static/favicon"]{
+      padding:28px;
+      object-fit:contain;
+    }
+  `;
+  document.head.append(style);
+
   const grid = document.querySelector(".postGrid");
   const pager = document.querySelector("[data-blog-pager]");
 
